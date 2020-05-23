a94902fc56f61076d3ec87bb0fec55beff60f464
Vinay Singh <vinaysingh1028@gmail.com>
2020-05-23 15:25:29 +0530

    Switch case

---
+++ switch.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int choice;
+	cin>>choice;
+	switch(choice)
+	{
+		case 1:
+			cout<<"case1";
+			break;
+		case 2:
+			cout<<"case2";
+			break;
+		case 3:
+			cout<<"case3";
+			break;
+		case 4:
+			cout<<"case4";
+			break;
+		case 5:
+			cout<<"case5";
+			break;
+		default:
+			cout<<"invalid choice";
+	}
+	return 0;
+}
