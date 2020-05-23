bffc0f1315d2d6e452c168c64c525ed9271a979a
Vinay Singh <vinaysingh1028@gmail.com>
2020-05-23 14:29:48 +0530

    Data types example

---
+++ DataTypes.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int integer;
+	long lvalue;
+	float floatvalue;
+	double doublevalue;
+	char ch;
+	cin>>integer>>lvalue>>floatvalue>>doublevalue>>ch;
+	cout<<integer<<"\n"<<lvalue<<"\n"<<floatvalue<<"\n"<<doublevalue<<"\n"<<ch<<endl;
+	return 0;
+	
+}
